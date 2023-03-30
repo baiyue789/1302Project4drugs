6fe11053fdf5dc2dbb9168617e31d860c73a7e2f
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-03-30 13:59:07 -0400

    Added new code to read and review the date for missing points
    So The guy doing this research is insane no missing data at all

--- codeItype.py
+++ codeItype.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+df = pd.read_csv("car.csv")
+print(df.isna().sum())
+#Checking data to see if there are any missing Data points
+df.dropna(inplace=True)
+#If missing data point delete said line
+print(len(df))
+#Checking new length of data types
