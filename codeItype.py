48136e86048f3f0884662e7e8207f144278521f2
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 14:11:31 -0400

    Update codeItype.py

--- codeItype.py
+++ codeItype.py
@@ -13,9 +13,11 @@ df.duplicated().sum()
 df = df.drop_duplicates()
 len(df)
 #Checking new length of data types
-gen_replace = {"2": 2, "3": 3, "4": 4, "5more" : 5, "more": 6}
+doors_persons = {"2": 2, "3": 3, "4": 4, "5more" : 5, "more": 6}
+buying_maint_safety = {"vhigh": 4, "high":3, "med":2, "low":1}
+lug = {"small":1, "med":2, "big":3}
 #apparently the numbers in the list is saved as str not ints
-df['doors'] = df['doors'].map(gen_replace)
+df['doors'] = df['doors'].map(doors_persons)
 doors_colm = df['doors']
 #there is a problem in calculating the mean since the more is not considered a number
 door_mean = float(doors_colm.mean())
