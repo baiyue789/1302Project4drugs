66a752793109aec5f73829d1a4990fa722c8d368
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 14:58:13 -0400

    pain

--- codeItype.py
+++ codeItype.py
@@ -18,8 +18,13 @@ buying_maint_safety = {"vhigh": 4, "high":3, "med":2, "low":1}
 lug = {"small":1, "med":2, "big":3}
 #apparently the numbers in the list is saved as str not ints
 df['doors'] = df['doors'].map(doors_persons)
+df['buying'] = df['buying'].map(buying_maint_safety)
 doors_colm = df['doors']
+buying_colm = df['buying']
 #there is a problem in calculating the mean since the more is not considered a number
 door_mean = float(doors_colm.mean())
 door_median = float(doors_colm.median())
+buying_mean= buying_colm.mean()
+buying_median = buying_colm.median()
 print(f'Mean:{door_mean} Median:{door_median}')
+print(f'Mean:{buying_mean} Median:{buying_median}')
