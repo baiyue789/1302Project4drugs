8a5e15563a1fe583ffc117dcc9a2bf03308660e8
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 15:45:29 -0400

    code unknown

--- codeItype.py
+++ codeItype.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 df = pd.read_csv("car.csv", index_col=False)
 #Checking new length of data types
 doors_persons = {"2": 2, "3": 3, "4": 4, "5more" : 5, "more": 6}
 buying_maint_safety = {"vhigh": 4, "high":3, "med":2, "low":1}
 lug = {"small":1, "med":2, "big":3}
 #apparently the numbers in the list is saved as str not ints
-df['doors'] = df['doors'].map(doors_persons)
+
 df['buying'] = df['buying'].map(buying_maint_safety)
+df['maint'] = df['maint'].map(buying_maint_safety)
+df['doors'] = df['doors'].map(doors_persons)
+df['persons'] = df['persons'].map(doors_persons)
+df['lug_boot'] = df['lug_boot'].map(lug)
+df['safety'] = df['safety'].map(buying_maint_safety)
 doors_colm = df['doors']
 buying_colm = df['buying']
 #there is a problem in calculating the mean since the more is not considered a number
@@ -19,9 +25,11 @@ buying_median = buying_colm.median()
 print(f'Mean number of doors:{door_mean} Median number of doors:{door_median}')
 print(f'Mean:{buying_mean} Median:{buying_median}')
 
-# create a scatter chart of columns 'A' and 'B'
-plt.scatter(df['doors'], df['persons'])
-plt.title("Scatter Chart")
-plt.xlabel("number of doors")
-plt.ylabel("People who can fit")
-plt.show()
+for sheet_name in df:
+    data = df[sheet_name]
+    fig, ax = plt.subplots()
+    ax.boxplot(data)
+    ax.set_xlabel('Data')
+    ax.set_ylabel('Value')
+    ax.set_title(f'Box Chart - {sheet_name}')
+    plt.show()
