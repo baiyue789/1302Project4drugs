8098adb2c3398fd2dcd6e0a951f631e0738e714a
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 13:40:26 -0400

    recalculations

--- codeItype.py
+++ codeItype.py
@@ -8,11 +8,18 @@ df.dropna(inplace=True)
 print(len(df))
 #Checking new length of data types
 doors_replace = {"2": 2, "3": 3, "4": 4, "5more" : 5}
+person_replace = {"2": 2, "4":4, "more": 6}
 #apparently the numbers in the list is saved as str not ints
 df['doors'] = df['doors'].map(doors_replace)
+df['persons'] = df['persons'].map(person_replace)
 
 doors_colm = df['doors']
 persons_colm = df['persons']
 #there is a problem in calculating the mean since the more is not considered a number
 door_mean = float(doors_colm.mean())
-print(f'Mean of doors is {door_mean}')
+door_median = float(doors_colm.median())
+print(f'Mean:{door_mean} Median:{door_median}')
+
+person_mean = float(persons_colm.mean())
+person_median = float(persons_colm.median())
+print(f'Mean:{person_mean}Median:{person_median}')
