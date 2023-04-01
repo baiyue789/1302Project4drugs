a35a10b70d4eeba7b38b137ed64442d8df04f21e
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 15:25:14 -0400

    change

--- codeItype.py
+++ codeItype.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 df = pd.read_csv("car.csv", index_col=False)
-df = df.drop(df[df['if'] == 'unacc'].index)
-print(len(df))
-df.to_csv('Cars_remove_unacc.csv', index=False)
 #Checking new length of data types
 doors_persons = {"2": 2, "3": 3, "4": 4, "5more" : 5, "more": 6}
 buying_maint_safety = {"vhigh": 4, "high":3, "med":2, "low":1}
@@ -18,5 +16,12 @@ door_mean = float(doors_colm.mean())
 door_median = float(doors_colm.median())
 buying_mean= buying_colm.mean()
 buying_median = buying_colm.median()
-print(f'Mean:{door_mean} Median:{door_median}')
+print(f'Mean number of doors:{door_mean} Median number of doors:{door_median}')
 print(f'Mean:{buying_mean} Median:{buying_median}')
+
+# create a scatter chart of columns 'A' and 'B'
+plt.scatter(df['doors'], df['persons'])
+plt.title("Scatter Chart")
+plt.xlabel("number of doors")
+plt.ylabel("People who can fit")
+plt.show()
