d2927639004a3d7435171072858ebdbe54d3a59a
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-04-01 09:10:19 -0400

    Testing simple stuff
    Oh the misery

--- codeItype.py
+++ codeItype.py
@@ -7,3 +7,12 @@ df.dropna(inplace=True)
 #If missing data point delete said line
 print(len(df))
 #Checking new length of data types
+doors_replace = {"2": 2, "3": 3, "4": 4, "5more" : 5}
+#apparently the numbers in the list is saved as str not ints
+df['doors'] = df['doors'].map(doors_replace)
+
+doors_colm = df['doors']
+persons_colm = df['persons']
+#there is a problem in calculating the mean since the more is not considered a number
+door_mean = float(doors_colm.mean())
+print(f'Mean of doors is {door_mean}')
